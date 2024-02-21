353863ba6407088b668149df0d17703ee2a8c767
eyorokumo <eyorokumodicksonemmanuel@gmail.com>
2024-02-21 22:48:07 +0100

    backendbabe

---
+++ backend_Home_Work.py
@@ -0,0 +1,26 @@
+# Question 1
+# Create variables and assign values
+name = "Emmanuel Dickson"
+school = "Python High School"
+class_ = "Grade 10"
+
+# Display personal details
+print("Personal Details:")
+print("Name:", )
+print("School:",)
+print("Class:",)
+print("Age:", 99)
+
+# Question 2
+# Create an empty list and display it
+empty_list = []
+print("Empty List:", empty_list)
+
+# Add items to the empty list
+empty_list.append("item1")
+empty_list.append("item2")
+empty_list.append("item3")
+
+print("Updated List:", empty_list)
+
+# 
